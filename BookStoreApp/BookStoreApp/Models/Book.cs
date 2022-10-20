62a84739b1c7b240d70016f9f672f7293a5dc949
keshav <keshav1555@gmail.com>
2022-10-20 16:09:24 +1100

    - Setup initial .NET MVC FileList - Created Model for Books - Created Controller for Books

---
+++ BookStoreApp/BookStoreApp/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public bool isReserved { get; set; }
+        public Book()
+        {
+
+        }
+    }
+}
